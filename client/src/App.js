f4848b3c4aeaaa72148a3ef341be24f09c5de0de
Praveen Kumar <praveenscience@gmail.com>
2022-03-27 06:49:44 +0100

    Connected FrontEnd with Backend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    Time: "Loading..."
+  };
+  componentDidMount() {
+    fetch("/api/time")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ Time: json });
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Khush Soni is here! :)</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.state.Time}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
